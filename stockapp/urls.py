86b098e4ed3474a729d1d985c4221cfb4430b1d3
Henu <hyun0404woo@gmail.com>
2021-08-02 04:38:54 +0900

    no url /data/

--- stockapp/urls.py
+++ stockapp/urls.py
@@ -6,16 +6,12 @@ from stockapp.views import *
 app_name = 'stockapp'
 
 router = routers.DefaultRouter()
-# router.register(r'kospi/',KospiAPIView.as_view())
-# router.register(r'chart/',ChartView.as_view())
 
 urlpatterns = [
-    # path('kospi/', kospi_list, name='kospilist'),
-    # path('kospi/<int:pk>', kospi_detail, name='kospidetail'),
     path('', include(router.urls)),
     path('stock/', StockAPIView.as_view()),
     path('chart/', ChartView.as_view()),
-    path('data/', getStockDataAll),
+    # path('data/', getStockDataAll),
     path('company/', getCompanylist),
     path('data/<str:name>', getCompanyDataAll),
     
