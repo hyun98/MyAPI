02943b1fcc95e5cd711b0ed0402b907138b052c7
Henu <hyun0404woo@gmail.com>
2021-08-04 21:06:04 +0900

    recieve context

--- accountapp/views.py
+++ accountapp/views.py
@@ -35,6 +35,7 @@ class LoginAPI(generics.GenericAPIView):
     serializer_class = LoginUserSerializer
 
     def post(self, request, *args, **kwargs):
+        print(request.data)
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         user = serializer.validated_data
